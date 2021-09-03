0c48daa09a96cfcd9831462cc238ba6c84748c34
Mauricio Robayo <rfmajo@gmail.com>
2021-09-03 11:37:33 -0500

    test: create parser
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- src/Parser/Parser.ts
+++ src/Parser/Parser.ts
@@ -1,54 +1,10 @@
 import { getDocument } from "pdfjs-dist/legacy/build/pdf.js";
 import fs from "fs/promises";
-import slugify from "slugify";
 
-type RefPrice = [string, number];
-type RefPriceParser = (content: string) => RefPrice | [string, RefPrice[]];
-
-const getPremiumRefPrice: RefPriceParser = (content: string): RefPrice => {
-  const premiumRefPriceRegExp =
-    /Precio\s+total\s+por\s+carga.*?(\d{0,3},?\d{3},\d{3})/;
-  const premiumRefPriceMatch = content.match(premiumRefPriceRegExp);
-
-  if (!premiumRefPriceMatch) {
-    console.log("Could not find precio total por carga!");
-    process.exit(1);
-  }
-
-  return ["premium", Number(premiumRefPriceMatch[1].replace(/,/g, ""))];
-};
-
-const getLowQualityRefPrice: RefPriceParser = (content: string): RefPrice => {
-  const lowQualityRefPriceRegExp =
-    /Precio\s+total\s+de\s+pasilla.*?(\d{0,3},?\d{0,3},?\d{3})/;
-  const lowQualityRefPriceMatch = content.match(lowQualityRefPriceRegExp);
-  if (!lowQualityRefPriceMatch) {
-    console.log("Could not find precio total de pasilla!");
-    process.exit(1);
-  }
-  return ["lowQuality", Number(lowQualityRefPriceMatch[1].replace(/,/g, ""))];
-};
-
-const getCitiesRefPrice: RefPriceParser = (
+export type RefPrice = [string, number];
+export type RefPriceParser = (
   content: string
-): [string, RefPrice[]] => {
-  const refPriceByCityMatch = content.matchAll(
-    /(armenia|bogot[aá]|bucaramanga|buga|chinchin[aá]|c[uú]cuta|igagu[eé]|manizales|medell[ií]n|neiva|pamplona|pasto|pereira|popay[aá]n|santa marta|valledupar)\s+?(\d{0,3}[,.]?\d{3}[,.]\d{3})/gi
-  );
-
-  const refPriceByCity: RefPrice[] = [...refPriceByCityMatch].map((match) => {
-    const refPrice = Number(match[2].replace(/,/g, ""));
-    const cityName = slugify(match[1], { lower: true });
-    if (Number.isNaN(refPrice)) {
-      throw new Error(
-        `IcpParser.getRefPriceByCity: could not parse refPrice '${match[2]}' for city '${match[1]}'`
-      );
-    }
-
-    return [cityName, refPrice];
-  });
-  return ["cities", refPriceByCity];
-};
+) => RefPrice | [string, RefPrice[]];
 
 export class IcpParser {
   async parse(
@@ -56,6 +12,7 @@ export class IcpParser {
     parsers: RefPriceParser[]
   ): Promise<Record<string, number | Record<string, number>>> {
     const content = await IcpParser.getContent(pdfPath);
+    console.log(content);
 
     return Object.fromEntries(
       parsers.map((parser) => {
@@ -91,6 +48,10 @@ export class IcpParser {
 }
 
 const parser = new IcpParser();
+
+function t(b: string) {
+  return b;
+}
 
 const parsers = [getLowQualityRefPrice, getPremiumRefPrice, getCitiesRefPrice];
 parser.parse("./pdf.pdf", parsers).then((json) => console.log(json));
