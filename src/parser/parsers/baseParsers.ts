bdfbddb80a7ed756fdd59f3c8ffeedbc1ea9e592
Mauricio Robayo <rfmajo@gmail.com>
2021-09-04 14:37:47 -0500

    chore: clean/remove/reorganize
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- src/parser/parsers/baseParsers.ts
+++ src/parser/parsers/baseParsers.ts
@@ -1,4 +1,16 @@
-import { createBaseRefPriceParser } from "./createRefPriceParser";
+function createBaseRefPriceParser(regExp: RegExp) {
+  return function (content: string): number {
+    const match = content.match(regExp);
+
+    if (!match) {
+      throw new Error(
+        `Failed to find pattern '${regExp.source} in content '${content}`
+      );
+    }
+
+    return Number(match[1].replace(/,/g, ""));
+  };
+}
 
 export const premiumRefPriceParser = createBaseRefPriceParser(
   /Precio total por carga.*?(\d{0,3},?\d{3},\d{3})/
