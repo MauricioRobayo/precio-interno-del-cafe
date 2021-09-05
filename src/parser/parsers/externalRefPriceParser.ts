0118d0975493352d1161164c8ee1dcffaf002307
Mauricio Robayo <rfmajo@gmail.com>
2021-09-05 06:20:23 -0500

    refactor: use hoc to create external price parser
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- src/parser/parsers/externalRefPriceParser.ts
+++ src/parser/parsers/externalRefPriceParser.ts
@@ -1,29 +1,18 @@
 import { ExternalRefPrice } from "../models/RefPriceStorage";
-import camelCase from "lodash/camelCase";
+import { createExtendedRefPriceParser } from "./createRefPriceParser";
 
-const contractsMap: { [k: string]: string } = {
-  primera: "nyCFirst",
-  segunda: "nyCSecond",
-  tercera: "nyCThird",
-};
+function keyMapper(key: string) {
+  return (
+    {
+      primera: "nyCFirst",
+      segunda: "nyCSecond",
+      tercera: "nyCThird",
+    }[key] || key
+  );
+}
+
 const regExp =
   /Cierre (primera|segunda|tercera) posici[oó]n.*?(\d{1,3}\.\d{2})/gi;
 
-export function externalRefPriceParser(content: string): ExternalRefPrice {
-  const matchArray = [...content.matchAll(regExp)];
-
-  if (matchArray.length === 0) {
-    throw new Error(
-      `Failed to find pattern '${regExp.source}' in content '${content}'`
-    );
-  }
-
-  return Object.fromEntries(
-    matchArray.map((match) => {
-      const key = camelCase(match[1]);
-      const value = Number(match[2].replace(/,/g, ""));
-
-      return [contractsMap[key], value];
-    })
-  ) as ExternalRefPrice;
-}
+export const externalRefPriceParser =
+  createExtendedRefPriceParser<ExternalRefPrice>(regExp, keyMapper);
