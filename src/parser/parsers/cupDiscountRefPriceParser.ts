4ac1b81f291f23c01e03cf6d64cf986fbe242ddb
Mauricio Robayo <rfmajo@gmail.com>
2021-09-05 06:13:25 -0500

    refactor: use hof to create cities parser
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- src/parser/parsers/cupDiscountRefPriceParser.ts
+++ src/parser/parsers/cupDiscountRefPriceParser.ts
@@ -1,24 +1,21 @@
 import { CupDiscount } from "../models/RefPriceStorage";
+import { createExtendedRefPriceParser } from "./createRefPriceParser";
 
 const regExp = /tipo (i q1|ii q2|iii q3) ([\d. ]+) /gi;
 
-const cupDiscountMap: { [k: string]: string } = {
-  "I Q1": "typeIQ1",
-  "II Q2": "typeIIQ2",
-  "III Q3": "typeIIIQ3",
-};
-
-export function cupDiscountRefPriceParser(content: string): CupDiscount {
-  const matchArray = [...content.matchAll(regExp)];
-
-  if (!matchArray) {
-    throw new Error("Could not parse content!");
-  }
-
-  return Object.fromEntries(
-    matchArray.map(([, key, value]) => [
-      cupDiscountMap[key],
-      value.split(" ").map(Number),
-    ])
-  ) as CupDiscount;
+function keyMapper(key: string) {
+  return (
+    {
+      "I Q1": "typeIQ1",
+      "II Q2": "typeIIQ2",
+      "III Q3": "typeIIIQ3",
+    }[key] || key
+  );
 }
+
+function valueParser(value: string) {
+  return value.split(" ").map(Number);
+}
+
+export const cupDiscountRefPriceParser =
+  createExtendedRefPriceParser<CupDiscount>(regExp, keyMapper, valueParser);
