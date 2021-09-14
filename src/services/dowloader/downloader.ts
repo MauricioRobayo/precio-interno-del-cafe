b0441febb2b7d82ecdbe077dd634918f2afb198f
Mauricio Robayo <rfmajo@gmail.com>
2021-09-13 21:06:53 -0500

    refactor(downloader): encapsulate into class
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- src/services/dowloader/downloader.ts
+++ src/services/dowloader/downloader.ts
@@ -13,28 +13,62 @@ type NotModifiedResponse = {
 const fileUrl =
   "https://federaciondecafeteros.org/app/uploads/2019/10/precio_cafe-1.pdf";
 
-export async function getFile(
-  eTag?: string | null
-): Promise<SuccessFulResponse | NotModifiedResponse> {
-  const headers: { [k: string]: string } = {};
+export class FileDownloader {
+  constructor(private eTag: string | null) {}
 
-  if (eTag) {
-    headers["If-None-Match"] = eTag;
-  }
+  async getFile(): Promise<SuccessFulResponse | NotModifiedResponse> {
+    const headers: { [k: string]: string } = {};
 
-  try {
-    const { data } = await axios.get<ArrayBuffer>(fileUrl, {
-      responseType: "arraybuffer",
-      headers,
-    });
-
-    return { status: 200, data };
-  } catch (err) {
-    if (axios.isAxiosError(err) && err.response?.status === 304) {
-      return { status: 304, data: null };
+    if (this.eTag) {
+      headers["If-None-Match"] = this.eTag;
     }
 
-    throw new Error(`downloadFile: get request failed ${err}`);
+    try {
+      const { data } = await axios.get<ArrayBuffer>(fileUrl, {
+        responseType: "arraybuffer",
+        headers,
+      });
+
+      return { status: 200, data };
+    } catch (err) {
+      if (axios.isAxiosError(err) && err.response?.status === 304) {
+        return { status: 304, data: null };
+      }
+
+      throw new Error(`downloadFile: get request failed ${err}`);
+    }
+  }
+
+  async getFileWithExponentialBackOff(
+    maxExecutionTime = 8 * 60 * 60 * 1000
+  ): Promise<ArrayBuffer | null> {
+    let delayMs = 0;
+    let retries = 0;
+    const startTime = Date.now();
+
+    // eslint-disable-next-line no-constant-condition
+    while (true) {
+      const response = await this.getFile();
+
+      if (isSuccessFulResponse(response)) {
+        return response.data;
+      }
+
+      const elapseTime = Date.now() - startTime;
+      if (elapseTime >= maxExecutionTime) {
+        console.log(`Max execution time reached. Tried for ${elapseTime}ms`);
+        return null;
+      }
+
+      retries = retries + 1;
+
+      const randomDeltaMs = randBetween(60 * 1000, 120 * 1000);
+      delayMs = (delayMs + randomDeltaMs) * 2;
+
+      console.log({ delayMs, retries });
+
+      await new Promise((resolve) => setTimeout(resolve, delayMs));
+    }
   }
 }
 
@@ -44,29 +78,9 @@ function isSuccessFulResponse(
   return response.status === 200;
 }
 
-export async function downloader(): Promise<ArrayBuffer | null> {
-  let delayMs = 0;
-  let retries = 0;
-  const maxExecutionTime = 8 * 60 * 60 * 1000;
-  const startTime = Date.now();
-
-  // eslint-disable-next-line no-constant-condition
-  while (true) {
-    const response = await getFile('"1a53c-5cbe534fe8477"'); // '"1a53c-5cbe534fe8477"'
-
-    if (isSuccessFulResponse(response)) {
-      return response.data;
-    }
-
-    const elapseTime = Date.now() - startTime;
-    if (elapseTime >= maxExecutionTime) {
-      console.log(`Max execution time reached. Tried for ${elapseTime}ms`);
-      return null;
-    }
-
-    retries = retries + 1;
-    delayMs = (delayMs + Math.floor(Math.random() * 60 * 1000)) * 2;
-    console.log({ delayMs, retries });
-    await new Promise((resolve) => setTimeout(resolve, delayMs));
-  }
+function randBetween(min: number, max: number): number {
+  return Math.floor(Math.random() * (max - min) + min);
 }
+
+const fd = new FileDownloader('"1a53c-5cbe534fe8477"');
+fd.getFileWithExponentialBackOff();
