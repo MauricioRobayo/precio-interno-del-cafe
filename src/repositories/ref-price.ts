cf1c75cd403f356b6e23023fecb2ef372ab548a2
Mauricio Robayo <rfmajo@gmail.com>
2021-09-14 05:54:54 -0500

    feat: add RefPriceRepository
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- src/repositories/ref-price.ts
+++ src/repositories/ref-price.ts
@@ -1,11 +1,13 @@
-import { Collection, MongoClient } from "mongodb";
+import { Collection, InsertOneResult, MongoClient } from "mongodb";
+import { RefPriceStorage } from "../models/ref-price";
 import { getEnvVars } from "../shared";
 
-const { MONGODB_URL, MONGODB_DB_NAME, MONGODB_COLLECTION } = getEnvVars([
-  "MONGODB_URL",
-  "DB_NAME",
+const { MONGODB_URI, MONGODB_DB_NAME, MONGODB_COLLECTION } = getEnvVars([
+  "MONGODB_URI",
+  "MONGODB_DB_NAME",
+  "MONGODB_COLLECTION",
 ]);
-const client = new MongoClient(MONGODB_URL);
+const client = new MongoClient(MONGODB_URI);
 
 export class RefPriceRepository {
   private client: Promise<MongoClient>;
@@ -13,9 +15,28 @@ export class RefPriceRepository {
     this.client = client.connect();
   }
 
-  protected async getCollection(): Promise<Collection<Document>> {
+  async getLatest(): Promise<RefPriceStorage> {
+    const collection = await this.getCollection();
+
+    const refPriceResult = await collection
+      .find()
+      .sort({ _id: -1 })
+      .limit(1)
+      .toArray();
+    return refPriceResult[0];
+  }
+
+  async save(
+    refPrice: RefPriceStorage
+  ): Promise<InsertOneResult<RefPriceStorage>> {
+    const collection = await this.getCollection();
+
+    return collection.insertOne(refPrice);
+  }
+
+  protected async getCollection(): Promise<Collection<RefPriceStorage>> {
     const client = await this.client;
     const db = client.db(MONGODB_DB_NAME);
-    return db.collection(MONGODB_COLLECTION);
+    return db.collection<RefPriceStorage>(MONGODB_COLLECTION);
   }
 }
