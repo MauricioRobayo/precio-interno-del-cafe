7afb3263aa599cbacbac125ba15bab11fddbc7b4
Mauricio Robayo <rfmajo@gmail.com>
2021-09-14 15:38:52 -0500

    chore: clean file and add logs
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- src/scripts/getRefPrice/index.ts
+++ src/scripts/getRefPrice/index.ts
@@ -5,39 +5,53 @@ import { parser } from "../../parser";
 const repo = new RefPriceRepository();
 
 async function getRefPrice(): Promise<void> {
-  const latestRefPrice = await repo.getLatest();
+  try {
+    const latestRefPrice = await repo.getLatest();
+    const fd = new FileDownloader(latestRefPrice?.etag);
+    const downloadResult = await fd.downloadFileWithExponentialBackOff();
+    const { retries, elapseTimeMs, status } = downloadResult;
 
-  const fd = new FileDownloader(
-    Math.random() > 0 ? undefined : latestRefPrice?.etag
-  );
-
-  const downloadResult = await fd.downloadFileWithExponentialBackOff();
-
-  console.log(downloadResult);
-
-  if (FileDownloader.isSuccessfulExponentialBackOffResult(downloadResult)) {
-    const { data, etag, lastModified } = downloadResult;
-
-    const parsedData = await parser(data);
-
-    const fileName = `${parsedData.parsedContent.date.slice(0, 10)}.pdf`;
-
-    await uploadFile(data, fileName);
-
-    await repo.save({
-      etag: etag,
-      lastModified: lastModified,
-      createdAt: Date.now(),
-      fileName,
-      pdfInfo: parsedData.pdfInfo,
-      refPrice: parsedData.parsedContent,
+    console.log("downloadResult", {
+      elapseTimeMs,
+      retries,
+      status,
     });
+
+    if (FileDownloader.isSuccessfulExponentialBackOffResult(downloadResult)) {
+      const { data, etag, lastModified } = downloadResult;
+      let fileName = "";
+
+      try {
+        const parsedData = await parser(data);
+        fileName = `${parsedData.parsedContent.date.slice(0, 10)}.pdf`;
+
+        await repo.save({
+          etag: etag,
+          lastModified: lastModified,
+          createdAt: Date.now(),
+          fileName,
+          pdfInfo: parsedData.pdfInfo,
+          refPrice: parsedData.parsedContent,
+        });
+      } catch (err) {
+        console.log("getRefPrice: parsing pdf file failed", err);
+      }
+
+      await uploadFile(data, fileName);
+
+      console.log("Process completed", {
+        etag,
+        fileName,
+        lastModified,
+      });
+
+      process.exit();
+    }
+  } catch (err) {
+    console.log("getRefPrice failed", err);
   }
+
+  process.exit(1);
 }
 
-getRefPrice()
-  .then(() => process.exit())
-  .catch((err) => {
-    console.log(err);
-    process.exit(1);
-  });
+getRefPrice();
