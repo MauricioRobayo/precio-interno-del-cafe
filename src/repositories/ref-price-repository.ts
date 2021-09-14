b1a3b419f3bab49f07708a47b2f63b3d9d4e8129
Mauricio Robayo <rfmajo@gmail.com>
2021-09-14 09:17:39 -0500

    chore: rename ref-price-repository file
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- src/repositories/ref-price-repository.ts
+++ src/repositories/ref-price-repository.ts
@@ -1,5 +1,5 @@
 import { Collection, InsertOneResult, MongoClient } from "mongodb";
-import { RefPriceStorage } from "../models/ref-price";
+import { RefPriceStorage } from "../models/ref-price-model";
 import { getEnvVars } from "../shared";
 
 const { MONGODB_URI, MONGODB_DB_NAME, MONGODB_COLLECTION } = getEnvVars([
@@ -11,19 +11,24 @@ const client = new MongoClient(MONGODB_URI);
 
 export class RefPriceRepository {
   private client: Promise<MongoClient>;
-  constructor(private collection: string) {
+  constructor() {
     this.client = client.connect();
   }
 
-  async getLatest(): Promise<RefPriceStorage> {
+  async getLatest(): Promise<RefPriceStorage | null> {
     const collection = await this.getCollection();
 
-    const refPriceResult = await collection
+    const refPriceArray = await collection
       .find()
       .sort({ _id: -1 })
       .limit(1)
       .toArray();
-    return refPriceResult[0];
+
+    if (refPriceArray.length === 0) {
+      return null;
+    }
+
+    return refPriceArray[0];
   }
 
   async save(
