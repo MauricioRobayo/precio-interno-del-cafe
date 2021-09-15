cef7e9bd8a1b4dcbb369374a299e2e707277a9c9
Mauricio Robayo <rfmajo@gmail.com>
2021-09-15 08:55:46 -0500

    fix(toGCS): exists returns an array
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- src/services/uploader/uploaders/toGCS.ts
+++ src/services/uploader/uploaders/toGCS.ts
@@ -1,38 +1,17 @@
 import { Storage } from "@google-cloud/storage";
-import { getEnvVars } from "../../../shared";
 
-const bucketName = "ref-price";
-const gcpEnvVars = getEnvVars([
-  "GCP_CREDENTIAL_TYPE",
-  "GCP_CREDENTIAL_PROJECT_ID",
-  "GCP_CREDENTIAL_PRIVATE_KEY_ID",
-  "GCP_CREDENTIAL_PRIVATE_KEY",
-  "GCP_CREDENTIAL_CLIENT_EMAIL",
-  "GCP_CREDENTIAL_CLIENT_ID",
-  "GCP_CREDENTIAL_AUTH_URI",
-  "GCP_CREDENTIAL_TOKEN_URI",
-  "GCP_CREDENTIAL_AUTH_PROVIDER_X509_CERT_URL",
-  "GCP_CREDENTIAL_CLIENT_X509_CERT_URL",
-]);
-
-const credentials = Object.fromEntries(
-  Object.entries(gcpEnvVars).map(([key, value]) => [
-    key.replace("GCP_CREDENTIAL_", "").toLowerCase(),
-    value,
-  ])
-);
-
-const storage = new Storage({ credentials });
+const storage = new Storage();
+const bucket = storage.bucket("ref-price");
 
 export async function toGCS(data: Buffer, destName: string): Promise<void> {
-  const fileExists = await storage.bucket(bucketName).file(destName).exists();
+  const file = bucket.file(destName);
+
+  const [fileExists] = await file.exists();
 
   if (fileExists) {
-    console.log(
-      `toGCP: File '${destName}' already in bucket, skipping upload.`
-    );
+    console.log(`toGCP: file '${destName}' already exists, skipping upload.`);
     return;
   }
 
-  await storage.bucket(bucketName).file(destName).save(data);
+  await file.save(data);
 }
